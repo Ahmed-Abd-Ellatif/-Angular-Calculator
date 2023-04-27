ac830e899c2f777912ba365eb639ccd7d26af48a
Ahmed Abd Ellatif <ahmedmahmoudfci@example.com>
2023-04-27 21:39:30 +0200

    Calculator App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculator';
+  resulte = '';
+
+  getAction(item: any) {
+    this.resulte += item;
+  }
+  equal() {
+    this.resulte = eval(this.resulte).toFixed(1);
+  }
+  reset() {
+    this.resulte = '';
+  }
 }
